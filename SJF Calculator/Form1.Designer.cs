7373ab1d59e98c6bba1a50772bfde58ef8d62089
MaazSiddiqui <siddiquimaaz88@gmail.com>
2024-06-18 15:40:54 +0500

    nrrd to fix gantt chart later

--- SJF Calculator/Form1.Designer.cs
+++ SJF Calculator/Form1.Designer.cs
@@ -48,6 +48,7 @@
             CloseBtn = new Button();
             CalculateSJF = new Button();
             ganttChartPanel = new Panel();
+            TestSampleData = new Button();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
@@ -59,7 +60,7 @@
             panel1.Controls.Add(label9);
             panel1.Location = new Point(0, 13);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1202, 57);
+            panel1.Size = new Size(1443, 57);
             panel1.TabIndex = 0;
             // 
             // label9
@@ -70,7 +71,7 @@
             label9.ForeColor = Color.White;
             label9.Location = new Point(497, 12);
             label9.Name = "label9";
-            label9.Size = new Size(297, 35);
+            label9.Size = new Size(379, 45);
             label9.TabIndex = 13;
             label9.Text = "SJF CALCULATOR";
             // 
@@ -93,7 +94,7 @@
             AvgWT.ForeColor = Color.White;
             AvgWT.Location = new Point(211, 33);
             AvgWT.Name = "AvgWT";
-            AvgWT.Size = new Size(45, 14);
+            AvgWT.Size = new Size(51, 18);
             AvgWT.TabIndex = 3;
             AvgWT.Text = "label4";
             // 
@@ -104,7 +105,7 @@
             AvgTAT.ForeColor = Color.White;
             AvgTAT.Location = new Point(211, 11);
             AvgTAT.Name = "AvgTAT";
-            AvgTAT.Size = new Size(45, 14);
+            AvgTAT.Size = new Size(51, 18);
             AvgTAT.TabIndex = 2;
             AvgTAT.Text = "label3";
             // 
@@ -115,7 +116,7 @@
             label2.ForeColor = Color.White;
             label2.Location = new Point(17, 33);
             label2.Name = "label2";
-            label2.Size = new Size(143, 14);
+            label2.Size = new Size(168, 18);
             label2.TabIndex = 1;
             label2.Text = "Average Waiting Time";
             // 
@@ -126,7 +127,7 @@
             label1.ForeColor = Color.White;
             label1.Location = new Point(17, 11);
             label1.Name = "label1";
-            label1.Size = new Size(172, 14);
+            label1.Size = new Size(205, 18);
             label1.TabIndex = 0;
             label1.Text = "Average Turn Around Time";
             // 
@@ -136,18 +137,18 @@
             dataGridView1.BorderStyle = BorderStyle.None;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.GridColor = Color.FromArgb(0, 63, 143);
-            dataGridView1.Location = new Point(553, 174);
+            dataGridView1.Location = new Point(654, 204);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(637, 384);
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(670, 409);
             dataGridView1.TabIndex = 3;
-            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.Location = new Point(17, 178);
             label5.Name = "label5";
-            label5.Size = new Size(180, 14);
+            label5.Size = new Size(213, 18);
             label5.TabIndex = 4;
             label5.Text = "Enter Number Of Processes";
             // 
@@ -156,7 +157,7 @@
             label6.AutoSize = true;
             label6.Location = new Point(17, 204);
             label6.Name = "label6";
-            label6.Size = new Size(119, 14);
+            label6.Size = new Size(139, 18);
             label6.TabIndex = 5;
             label6.Text = "Enter Process IDs";
             // 
@@ -165,7 +166,7 @@
             label7.AutoSize = true;
             label7.Location = new Point(17, 233);
             label7.Name = "label7";
-            label7.Size = new Size(117, 14);
+            label7.Size = new Size(139, 18);
             label7.TabIndex = 6;
             label7.Text = "Enter Arrival Time";
             // 
@@ -174,7 +175,7 @@
             label8.AutoSize = true;
             label8.Location = new Point(17, 262);
             label8.Name = "label8";
-            label8.Size = new Size(111, 14);
+            label8.Size = new Size(132, 18);
             label8.TabIndex = 7;
             label8.Text = "Enter Burst Time";
             // 
@@ -254,18 +255,34 @@
             // 
             // ganttChartPanel
             // 
-            ganttChartPanel.Location = new Point(553, 97);
+            ganttChartPanel.Location = new Point(654, 109);
             ganttChartPanel.Name = "ganttChartPanel";
-            ganttChartPanel.Size = new Size(637, 61);
+            ganttChartPanel.Size = new Size(670, 61);
             ganttChartPanel.TabIndex = 13;
+            // 
+            // TestSampleData
+            // 
+            TestSampleData.BackColor = Color.FromArgb(51, 51, 51);
+            TestSampleData.BackgroundImageLayout = ImageLayout.Zoom;
+            TestSampleData.FlatStyle = FlatStyle.Flat;
+            TestSampleData.ForeColor = SystemColors.Control;
+            TestSampleData.Location = new Point(289, 311);
+            TestSampleData.Name = "TestSampleData";
+            TestSampleData.Size = new Size(103, 29);
+            TestSampleData.TabIndex = 14;
+            TestSampleData.Text = "Test";
+            TestSampleData.UseVisualStyleBackColor = false;
+            TestSampleData.Click += TestSampleData_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 14F);
+            AutoScaleDimensions = new SizeF(9F, 18F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(1202, 570);
+            ClientSize = new Size(1443, 724);
+            Controls.Add(TestSampleData);
             Controls.Add(ganttChartPanel);
+            Controls.Add(dataGridView1);
             Controls.Add(CalculateSJF);
             Controls.Add(CloseBtn);
             Controls.Add(BurstTime);
@@ -276,14 +293,12 @@
             Controls.Add(label7);
             Controls.Add(label6);
             Controls.Add(label5);
-            Controls.Add(dataGridView1);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Font = new Font("Verdana", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             Text = "Form1";
-            Load += Form1_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             panel2.ResumeLayout(false);
@@ -314,5 +329,6 @@
         private Label label9;
         private Button CalculateSJF;
         private Panel ganttChartPanel;
+        private Button TestSampleData;
     }
 }
--- SJF Calculator/Form1.cs
+++ SJF Calculator/Form1.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Text;
 using System.Windows.Forms;
 
 namespace SJF_Calculator
@@ -11,28 +12,17 @@ namespace SJF_Calculator
     public partial class Form1 : Form
     {
         private List<Process> processes = new List<Process>();
+        private List<Process> scheduledProcesses = new List<Process>(); // Moved to class level
         private double avgTurnAroundTime = 0;
-        private double avgWaitingTime = 1;
+        private double avgWaitingTime = 0;
 
         public Form1()
         {
             InitializeComponent();
-            this.FormBorderStyle = FormBorderStyle.None; // Remove border to apply custom rounded border
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.StartPosition = FormStartPosition.CenterScreen;
             this.Paint += new PaintEventHandler(Form1_Paint);
-            this.StartPosition = FormStartPosition.CenterScreen; // Center the form on the screen
-
-            // Add event handler for the close button
-            this.CloseBtn.Click += new EventHandler(CloseBtn_Click);
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
+            CloseBtn.Click += new EventHandler(CloseBtn_Click);
         }
 
         private void CalculateSJF_Click(object sender, EventArgs e)
@@ -40,9 +30,9 @@ namespace SJF_Calculator
             ClearForm();
 
             int n;
-            if (!int.TryParse(NoOfProcesses.Text, out n))
+            if (!int.TryParse(NoOfProcesses.Text, out n) || n <= 0)
             {
-                MessageBox.Show("Please enter a valid number of processes.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Please enter a valid positive number of processes.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
@@ -76,11 +66,11 @@ namespace SJF_Calculator
                 return;
             }
 
-            // Sort processes by Arrival Time for initial scheduling
             processes = processes.OrderBy(p => p.ArrivalTime).ToList();
 
             int currentTime = 0;
-            var scheduledProcesses = new List<Process>();
+            scheduledProcesses.Clear(); // Clear the scheduled processes list
+
             while (processes.Any())
             {
                 var availableProcesses = processes.Where(p => p.ArrivalTime <= currentTime).ToList();
@@ -105,24 +95,26 @@ namespace SJF_Calculator
                 scheduledProcesses.Add(nextProcess);
             }
 
-            // Calculate average times
             avgTurnAroundTime = scheduledProcesses.Average(p => p.TurnAroundTime);
             avgWaitingTime = scheduledProcesses.Average(p => p.WaitingTime);
 
-            // Sort processes by Process ID to display in the correct order
             scheduledProcesses = scheduledProcesses.OrderBy(p => p.ProcessID).ToList();
 
             // Update DataGridView
-            dataGridView1.DataSource = null;
             dataGridView1.DataSource = scheduledProcesses;
-
-            // Update average time labels
             AvgTAT.Text = avgTurnAroundTime.ToString("F2");
             AvgWT.Text = avgWaitingTime.ToString("F2");
 
-            // Draw the Gantt chart
-            processes = scheduledProcesses; // Ensure Gantt chart uses the correct order
-            DrawGanttChart();
+            // Debug messages
+            if (scheduledProcesses.Count > 0)
+            {
+                MessageBox.Show($"Processes scheduled: {scheduledProcesses.Count}", "Debug", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DrawGanttChart();
+            }
+            else
+            {
+                MessageBox.Show("No processes to display in Gantt chart.", "No Processes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private bool ValidateInputs(int n, string[] processIds, string[] arrivalTimes, string[] burstTimes)
@@ -135,27 +127,36 @@ namespace SJF_Calculator
             dataGridView1.DataSource = null;
             AvgTAT.Text = "0";
             AvgWT.Text = "0";
-            ganttChartPanel.Invalidate();
+            ganttChartPanel.BackgroundImage = null; // Clear gantt chart panel background
         }
 
         private void DrawGanttChart()
         {
+            if (scheduledProcesses.Count == 0)
+            {
+                MessageBox.Show("No processes to display in Gantt chart.", "No Processes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            int totalBurstTime = scheduledProcesses.Sum(p => p.BurstTime);
             Bitmap bmp = new Bitmap(ganttChartPanel.Width, ganttChartPanel.Height);
+
             using (Graphics g = Graphics.FromImage(bmp))
             {
                 g.Clear(Color.White);
+
                 int currentX = 0;
                 int processHeight = ganttChartPanel.Height / 3;
-                int timeHeight = ganttChartPanel.Height / 3;
 
-                foreach (var process in processes.OrderBy(p => p.CompletionTime - p.BurstTime)) // Order by start time
+                foreach (var process in scheduledProcesses.OrderBy(p => p.CompletionTime - p.BurstTime))
                 {
-                    int processWidth = (process.BurstTime * ganttChartPanel.Width) / processes.Sum(p => p.BurstTime);
+                    int processWidth = (int)Math.Round((double)process.BurstTime / totalBurstTime * ganttChartPanel.Width);
+
                     g.FillRectangle(new SolidBrush(Color.FromArgb(63, 81, 181)), currentX, 0, processWidth, processHeight);
                     g.DrawRectangle(Pens.Black, currentX, 0, processWidth, processHeight);
                     g.DrawString("P" + process.ProcessID, this.Font, Brushes.White, currentX + (processWidth / 2) - 10, 10);
-
                     g.DrawString(process.CompletionTime.ToString(), this.Font, Brushes.Black, currentX + processWidth - 10, processHeight + 10);
+
                     currentX += processWidth;
                 }
             }
@@ -171,17 +172,58 @@ namespace SJF_Calculator
             GraphicsPath path = new GraphicsPath();
             path.AddArc(bounds.X, bounds.Y, radius, radius, 180, 90);
             path.AddArc(bounds.X + bounds.Width - radius, bounds.Y, radius, radius, 270, 90);
-            path.AddArc(bounds.X + bounds.Width - radius, bounds
-            .Height - radius, radius, radius, 0, 90);
+            path.AddArc(bounds.X + bounds.Width - radius, bounds.Height - radius, radius, radius, 0, 90);
             path.AddArc(bounds.X, bounds.Y + bounds.Height - radius, radius, radius, 90, 90);
             path.CloseAllFigures();
             this.Region = new Region(path);
         }
 
-        // Event handler for Close button
         private void CloseBtn_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Application.Exit();
+        }
+
+        private void GenerateSampleData()
+        {
+            Random random = new Random();
+            int numProcesses = random.Next(3, 6); // Generate between 3 to 5 processes
+
+            StringBuilder processIds = new StringBuilder();
+            StringBuilder arrivalTimes = new StringBuilder();
+            StringBuilder burstTimes = new StringBuilder();
+
+            for (int i = 0; i < numProcesses; i++)
+            {
+                int processId = i + 1;
+                int arrivalTime = random.Next(0, 10); // Arrival time between 0 to 9
+                int burstTime = random.Next(1, 10);   // Burst time between 1 to 9
+
+                processIds.Append(processId);
+                arrivalTimes.Append(arrivalTime);
+                burstTimes.Append(burstTime);
+
+                if (i < numProcesses - 1)
+                {
+                    processIds.Append(",");
+                    arrivalTimes.Append(",");
+                    burstTimes.Append(",");
+                }
+            }
+
+            // Assign generated data to respective text boxes
+            NoOfProcesses.Text = numProcesses.ToString();
+            ProcessIDs.Text = processIds.ToString();
+            ArrivalTime.Text = arrivalTimes.ToString();
+            BurstTime.Text = burstTimes.ToString();
+
+            MessageBox.Show("Sample data generated successfully.");
+        }
+
+        private void TestSampleData_Click(object sender, EventArgs e)
+        {
+            GenerateSampleData();
+            // Optionally, you can also call CalculateSJF_Click() here if you want immediate calculation
+            // CalculateSJF_Click(sender, e);
         }
     }
 
